fcf9b54b2738dc7c2c4b59280775fdff6d2207b5
Firstname Lastnamevanhai260300 <your_email@youremail.com>
2020-09-27 19:27:16 +0700

    first commit

---
+++ app/src/main/java/com/nvh/login_logup/LogupActivity.java
@@ -0,0 +1,13 @@
+package com.nvh.login_logup;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LogupActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
